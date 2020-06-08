4759cb1d8f103b7c743c0bd7b3fc011987f4e2e0
nghiatc <congnghia0609@gmail.com>
2020-06-08 21:09:50 +0700

    update code

--- src/main/java/com/ntc/kafka/util/KConfig.java
+++ src/main/java/com/ntc/kafka/util/KConfig.java
@@ -20,6 +20,8 @@ import com.ntc.configer.NConfig;
 import java.util.Properties;
 import java.util.*;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.streams.StreamsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,8 +48,45 @@ public class KConfig {
                     props.put(cname, nvalue);
                 }
             }
+            props.put(ConsumerConfig.CLIENT_ID_CONFIG, name + "_consumer_" + UUID.randomUUID().toString());
         } catch (Exception e) {
             log.error("getConsumeConfig " + e.toString(), e);
+        }
+        return props;
+    }
+    
+    public static Properties getProduceConfig(String name) {
+        Properties props = new Properties();
+        try {
+            Set<String> setName = ProducerConfig.configNames();
+            for (String pname : setName) {
+                String nkey = name + PRODUCER_PREFIX + pname;
+                Object nvalue = NConfig.getConfig().containsKey(nkey) ? NConfig.getConfig().getProperty(nkey) : null;
+                if (nvalue != null) {
+                    props.put(pname, nvalue);
+                }
+            }
+            props.put(ProducerConfig.CLIENT_ID_CONFIG, name + "_producer_" + UUID.randomUUID().toString());
+        } catch (Exception e) {
+            log.error("getProduceConfig " + e.toString(), e);
+        }
+        return props;
+    }
+    
+    public static Properties getStreamConfig(String name) {
+        Properties props = new Properties();
+        try {
+            Set<String> setName = StreamsConfig.configDef().names();
+            for (String pname : setName) {
+                String nkey = name + STREAM_PREFIX + pname;
+                Object nvalue = NConfig.getConfig().containsKey(nkey) ? NConfig.getConfig().getProperty(nkey) : null;
+                if (nvalue != null) {
+                    props.put(pname, nvalue);
+                }
+            }
+            props.put(StreamsConfig.CLIENT_ID_CONFIG, name + "_stream_" + UUID.randomUUID().toString());
+        } catch (Exception e) {
+            log.error("getProduceConfig " + e.toString(), e);
         }
         return props;
     }
@@ -56,8 +95,163 @@ public class KConfig {
         try {
             //test1();
             
-            test2();
+            //test2();
             
+            //test3();
+            
+            //test4();
+            
+            //test5();
+            
+            test6();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void test6() {
+        try {
+            Properties props = getStreamConfig("worker");
+            System.out.println(props.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+    application.id
+    bootstrap.servers
+    replication.factor
+    state.dir
+    cache.max.bytes.buffering
+    client.id
+    default.deserialization.exception.handler
+    default.key.serde
+    default.production.exception.handler
+    default.timestamp.extractor
+    default.value.serde
+    num.standby.replicas
+    num.stream.threads
+    max.task.idle.ms
+    processing.guarantee
+    security.protocol
+    topology.optimization
+    application.server
+    buffered.records.per.partition
+    built.in.metrics.version
+    commit.interval.ms
+    connections.max.idle.ms
+    metadata.max.age.ms
+    metrics.num.samples
+    metric.reporters
+    metrics.recording.level
+    metrics.sample.window.ms
+    partition.grouper
+    poll.ms
+    receive.buffer.bytes
+    reconnect.backoff.ms
+    reconnect.backoff.max.ms
+    retries
+    retry.backoff.ms
+    request.timeout.ms
+    rocksdb.config.setter
+    send.buffer.bytes
+    state.cleanup.delay.ms
+    upgrade.from
+    windowstore.changelog.additional.retention.ms
+     */
+    private static void test5() {
+        try {
+            Set<String> setName = StreamsConfig.configDef().names();
+            for (String sname : setName) {
+                System.out.println(sname);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void test4() {
+        try {
+            Properties props = getProduceConfig("worker");
+            System.out.println(props.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+    bootstrap.servers
+    client.dns.lookup
+    buffer.memory
+    retries
+    acks
+    compression.type
+    batch.size
+    linger.ms
+    delivery.timeout.ms
+    client.id
+    send.buffer.bytes
+    receive.buffer.bytes
+    max.request.size
+    reconnect.backoff.ms
+    reconnect.backoff.max.ms
+    retry.backoff.ms
+    max.block.ms
+    request.timeout.ms
+    metadata.max.age.ms
+    metadata.max.idle.ms
+    metrics.sample.window.ms
+    metrics.num.samples
+    metrics.recording.level
+    metric.reporters
+    max.in.flight.requests.per.connection
+    key.serializer
+    value.serializer
+    connections.max.idle.ms
+    partitioner.class
+    interceptor.classes
+    security.protocol
+    security.providers
+    ssl.protocol
+    ssl.provider
+    ssl.cipher.suites
+    ssl.enabled.protocols
+    ssl.keystore.type
+    ssl.keystore.location
+    ssl.keystore.password
+    ssl.key.password
+    ssl.truststore.type
+    ssl.truststore.location
+    ssl.truststore.password
+    ssl.keymanager.algorithm
+    ssl.trustmanager.algorithm
+    ssl.endpoint.identification.algorithm
+    ssl.secure.random.implementation
+    sasl.kerberos.service.name
+    sasl.kerberos.kinit.cmd
+    sasl.kerberos.ticket.renew.window.factor
+    sasl.kerberos.ticket.renew.jitter
+    sasl.kerberos.min.time.before.relogin
+    sasl.login.refresh.window.factor
+    sasl.login.refresh.window.jitter
+    sasl.login.refresh.min.period.seconds
+    sasl.login.refresh.buffer.seconds
+    sasl.mechanism
+    sasl.jaas.config
+    sasl.client.callback.handler.class
+    sasl.login.callback.handler.class
+    sasl.login.class
+    enable.idempotence
+    transaction.timeout.ms
+    transactional.id
+     */
+    private static void test3() {
+        try {
+            Set<String> setName = ProducerConfig.configNames();
+            for (String pname : setName) {
+                System.out.println(pname);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
